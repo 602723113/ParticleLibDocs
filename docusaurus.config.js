746a9743973cea19c889949749355622a907900c
Zoyn <602723113@qq.com>
2023-01-23 14:48:25 +0800

    更新主页

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,8 +6,8 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'ParticleLib',
+  tagline: '一个随便写的但是还挺稳的粒子特效库',
   url: 'https://your-docusaurus-test-site.com',
   baseUrl: '/',
   onBrokenLinks: 'throw',
